016a1cc3fdf3ea31a02c21b75968ca955ec09c51
Felipe_Augusto1828 <felipesilva.cnt@gmail.com>
2022-01-10 11:46:42 -0500

    script correction: Port scan

--- portscan.sh
+++ portscan.sh
@@ -3,10 +3,11 @@
 if ["$1" == ""]
 then
 	echo "PORTSCAN NETWORK"
-	echo "Modo de uso: $0 rede"
-	echo "Exemplo: $0 172.16.1"
+	echo "Modo de uso: $0 rede porta"
+	echo "Exemplo: $0 172.16.1 80"
 else
-	for ip in {1..254};
-	do hping3 -S -p 80 -c 1 $1.$ip 2> /dev/null | grep "flags=SA";
+for ip in {1..254};
+do 
+hping3 -S -p $2 -c 1 $1.$ip 2> /dev/null | grep "flags=SA";
 done
 fi
